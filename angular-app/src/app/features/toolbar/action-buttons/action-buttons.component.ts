86c3e347b8396de5f783fdaf233b30925b0fd736
Father Abraham <abembecker@gmail.com>
2024-01-14 06:31:01 -0500

    chore: added angular material, custom theme, and user auth mock service.

---
+++ angular-app/src/app/features/toolbar/action-buttons/action-buttons.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {UserAuthService} from "../services/user-auth.service";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-action-buttons',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './action-buttons.component.html',
+  styleUrl: './action-buttons.component.scss'
+})
+export class ActionButtonsComponent {
+  constructor(public userAuthService: UserAuthService) {
+  }
+}
--- angular-app/src/app/features/toolbar/gitcoin/gitcoin.component.ts
+++ angular-app/src/app/features/toolbar/gitcoin/gitcoin.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-gitcoin',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './gitcoin.component.html',
   styleUrl: './gitcoin.component.scss'
 })
